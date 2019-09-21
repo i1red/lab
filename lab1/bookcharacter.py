4487c04a703090810d892376091c514e2d97460c
Ivan Krasnoshchok <46425174+i1red@users.noreply.github.com>
2019-09-21 20:22:42 +0300

    Update bookcharacter.py

--- lab1/bookcharacter.py
+++ lab1/bookcharacter.py
@@ -10,39 +10,31 @@ class CharacterRole(Enum):
     SECONDARY = 1
     MAIN = 2
 
-
 class BookCharacter:
     def __init__(self, names: list, reffered_books: list):
         self._names = list(names)
         self._reffered_books = [(book, role) for book, role in reffered_books]
 
-
     def __repr__(self):
         return f'BookCharacter({reprlib.repr(self._names)}, {reprlib.repr(self._reffered_books)})'
-
 
     def __str__(self):
         ref_books_str = [f'''"{book.title}" by {', '.join(book.authors)}'''  for book, role in self._reffered_books]
         return f"Book character, known as {', '.join(self.names)}. Reffered books: {'; '.join(ref_books_str)}"
 
-
     @property
     def names(self):
         return list(self._names)
-
 
     @property
     def reffered_books(self):
         return list(self._reffered_books)
 
-
     def add_name(self, name: str):
         self._names.append(name)
 
-
     def add_book(self, reffered_book: Book, role):
         self._reffered_books.append((reffered_book, role))
-
 
     def serialize_books(self):
         return [book for book, role in self._reffered_books
