156180a4dd90ea3dce13094c03da57fd37095842
i1red <ivankras67@gmail.com>
2019-11-26 22:49:31 +0200

    Changed demo

--- project.lab3/main.py
+++ project.lab3/main.py
@@ -14,10 +14,30 @@ def create_training(frames_number):
     return training
 
 
+def create_new_user_demo(username, password):
+    def_user = user_id.UserID.register(username, password)
+    def_user.create_recognizer(create_training(50))
+
+    unlocked_with_face = False
+    for img in cam_tools.camera_read(time_limit=3):
+        face = cam_tools.detect_face(img)
+
+        if face is not None:
+            unlocked_with_face = def_user.face_unlock(face)
+
+        if unlocked_with_face:
+            break
+
+    if unlocked_with_face:
+        print(f'Recognized {def_user.username}')
+
+    print(def_user.password_unlock(password + 'a'))
+    print(def_user.password_unlock(password))
+
+
 if __name__ == '__main__':
     password = '12345'
-    ivan = user_id.UserID.register('ivan2', password)
-    ivan.create_recognizer(create_training(50))
+    ivan = user_id.UserID('ivan')
 
     unlocked_with_face = False
     for img in cam_tools.camera_read(time_limit=3):
