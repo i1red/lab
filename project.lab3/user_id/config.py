0941e71be0a984416f963cb829464a6b4b859f6a
i1red <ivankras67@gmail.com>
2019-11-26 15:12:27 +0200

    Updated config

--- project.lab3/user_id/config.py
+++ project.lab3/user_id/config.py
@@ -1,6 +1,9 @@
 from cv2.data import haarcascades
+from cv2.face import LBPHFaceRecognizer_create
 
 
+DEFAULT_CAMERA = 0
 FACE_SIZE = (140, 140)
 USERS_DIR = '../users/'
 HAARCASCADES = haarcascades
+CREATE_RECOGNIZER = LBPHFaceRecognizer_create
