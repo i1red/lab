d0d3635019015a566f02643dde9a9623d3debbae
i1red <ivankras67@gmail.com>
2019-12-08 21:12:36 +0200

    Updated

--- lab2/main.py
+++ lab2/main.py
@@ -1,18 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from sources.main_window import MainWindow
-
-
-class App:
-    def __init__(self):
-        self.app = QApplication(sys.argv)
-        self.mainWindow = MainWindow('Linux File Manager')
-
-    def run(self):
-        self.mainWindow.show()
-        sys.exit(self.app.exec_())
+from mainwindow import MainWindow
 
 
 if __name__ == '__main__':
-    app = App()
-    app.run()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
