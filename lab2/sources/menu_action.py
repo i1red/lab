bf0fde50d624cb9b360d6a5a3c269c1f9288f940
i1red <ivankras67@gmail.com>
2019-12-08 21:02:12 +0200

    Deleting old structure project

--- lab2/sources/menu_action.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QAction
-
-
-class MenuAction(QAction):
-    def __init__(self, *args, clicked):
-        super().__init__(*args)
-
-        self.triggered.connect(clicked)
