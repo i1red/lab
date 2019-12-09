098e564055f502d0a03de361689c0a0eb2002c06
i1red <ivankras67@gmail.com>
2019-12-09 09:45:20 +0200

    Added setters to change file permissions

--- lab2/filestats.py
+++ lab2/filestats.py
@@ -3,19 +3,29 @@ import grp
 import pwd
 import time
 import stat
+import subprocess
 
 
 class FileStats:
     def __init__(self, path):
         fstats = os.stat(path)
         permissions = stat.filemode(fstats.st_mode)
+        splitPath = path.split('/')
+
+        self._name = splitPath[-1]
+        splitPath.pop()
+        self._dir = '/' + '/'.join(splitPath) + ('/' if len(splitPath) > 0 else '')
+
         self._isDir = permissions[0] == 'd'
-        self._permissionsOwner = permissions[1:4]
-        self._permissionsGroup = permissions[4:7]
-        self._permissionsOthers = permissions[7:]
+        self._permissionsOwner = tuple(permissions[1:4])
+        self._permissionsGroup = tuple(permissions[4:7])
+        self._permissionsOthers = tuple(permissions[7:])
+
         self._ownerName = pwd.getpwuid(fstats.st_uid).pw_name
         self._groupName = grp.getgrgid(fstats.st_gid).gr_name
+
         self._fileSize = FileStats._convertFileSize(FileStats._dirSize(path))
+
         self._lastAccessed = time.ctime(fstats.st_atime)
         self._lastModified = time.ctime(fstats.st_mtime)
 
@@ -24,16 +34,47 @@ class FileStats:
         return self._isDir
 
     @property
+    def name(self):
+        return self._name
+
+    def _path(self):
+        return self._dir + self.name
+
+    @property
     def permissionsOwner(self):
         return self._permissionsOwner
 
+    @permissionsOwner.setter
+    def permissionsOwner(self, permissions):
+        if permissions != self._permissionsOwner:
+            try:
+                subprocess.run([f'chmod u+{"".join(permissions)}', self._path()], check=True)
+            except subprocess.CalledProcessError:
+                pass
+
     @property
     def permissionsGroup(self):
-        return self._permissionsOthers
+        return self._permissionsGroup
+
+    @permissionsGroup.setter
+    def permissionsGroup(self, permissions):
+        if permissions != self._permissionsGroup:
+            try:
+                subprocess.run([f'chmod g+{"".join(permissions)}', self._path()], check=True)
+            except subprocess.CalledProcessError:
+                pass
 
     @property
     def permissionsOthers(self):
         return self._permissionsOthers
+
+    @permissionsOthers.setter
+    def permissionsOthers(self, permissions):
+        if permissions != self._permissionsOthers:
+            try:
+                subprocess.run([f'chmod o+{"".join(permissions)}', self._path()], check=True)
+            except subprocess.CalledProcessError:
+                pass
 
     @property
     def groupName(self):
