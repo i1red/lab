0371c9362da09d6f1eac3a78990da84de367cbe3
i1red <ivankras67@gmail.com>
2019-12-11 16:29:47 +0200

    Added demos

--- lab1/demo.py
+++ lab1/demo.py
@@ -1,7 +1,7 @@
 import random
 import string
+from priorityqueue import PriorityQueue
 from bookcharacter import *
-
 
 RAND_SEQ_SIZE_BOTTOM = 3
 RAND_SEQ_SIZE_TOP = 12
@@ -9,7 +9,8 @@ RAND_SEQ_SIZE_TOP = 12
 
 def random_str():
     length = random.randint(RAND_SEQ_SIZE_BOTTOM, RAND_SEQ_SIZE_TOP)
-    return random.choice(string.ascii_uppercase) + ''.join(string.ascii_lowercase for _ in range(1, length))
+    return random.choice(string.ascii_uppercase) + \
+           ''.join(random.choice(string.ascii_lowercase) for _ in range(1, length))
 
 
 def random_int():
@@ -39,4 +40,55 @@ def random_book_character():
 
 
 def random_obj():
-    return random.choice([random_str, random_int, random_list, random_tuple, random_book, random_book_character])
+    return random.choice([random_str, random_int, random_list, random_tuple, random_book, random_book_character])()
+
+
+def demo_basic():
+    qu = PriorityQueue()
+
+    books = [Book(random_str(), [random_str(), random_str()], random.randint(-40, 1920),
+                  random.randint(3, 9821), random_str()) for _ in range(20)]
+
+    prices = [random.randint(3, 582) for _ in range(20)]
+
+    for pair in zip(books, prices):
+        qu.put(pair)
+
+    random_character = BookCharacter(['random character'], [])
+
+    while qu:
+        book, price = qu.get()
+        print(book)
+        random_character.add_book(book, CharacterRole(price % 3))
+
+    print(random_character)
+    print(repr(random_character))
+
+
+def demo_priority_queue():
+    print('DEMO OF USING PRIORITY QUEUE WITH DIFFERENT TYPES')
+
+    key_type = random.choice([random_int, random_str])
+    puts_num = random.randint(20, 30)
+    gets_num = random.randint(5, puts_num - 1)
+
+    pq = PriorityQueue()
+    print('PRIORITY QUEUE CURRENT STATE: ')
+    print(pq)
+
+    for _ in range(puts_num):
+        key, obj = key_type(), random_obj()
+
+        print()
+        print(f'PUT KEY={key}, OBJECT={obj}')
+        pq.put((key, obj))
+
+        print('PRIORITY QUEUE CURRENT STATE: ')
+        print(pq)
+
+    for _ in range(gets_num):
+        key, obj = pq.get()
+        print()
+        print(f'GET KEY={key}, OBJECT={obj}')
+        print('PRIORITY QUEUE CURRENT STATE: ')
+        print(pq)
--- lab1/main.py
+++ lab1/main.py
@@ -1,32 +1,5 @@
-from priorityqueue import PriorityQueue
-from random import randint
-from bookcharacter import *
-
-
-#1st list 5th task: priority queue ***
-#Based on sorted list; AVL tree - additional **
-#Priority queue adapts cantainer with insert and popleft methods
-#2nd list 10th task: books and book characters ***
-#Using unit testing
+from demo import demo_basic, demo_priority_queue
 
 
 if __name__ == '__main__':
-    qu = PriorityQueue()
-
-    books = [Book(f'{chr(randint(65, 90))}{chr(randint(97, 122))}', [f'{chr(randint(65, 90))}',
-             f'{randint(65, 90)}'], randint(-40, 1920), randint(3, 9821), f'annotation{i}') for i in range(20)]
-
-    prices = [randint(3, 582) for _ in range(20)]
-
-    for pair in zip(books, prices):
-        qu.put(pair)
-
-    random_character = BookCharacter(['randomist'], [])
-
-    while qu:
-        book, price = qu.get()
-        print(book)
-        random_character.add_book(book, CharacterRole(price % 3))
-
-    print(random_character)
-    print(repr(random_character))
+    demo_priority_queue()
